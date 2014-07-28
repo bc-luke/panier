0c38be0da25a51cae861408cc436251e87708e85
Luke Eller <luke.eller@bigcommerce.com>
2014-07-28 16:07:00 +1000

    Clean up dependencies, require statements

--- lib/panier.rb
+++ lib/panier.rb
@@ -4,14 +4,22 @@ require 'active_support/dependencies'
 
 require 'panier/version'
 
+# domain
 require 'panier/domain/line_item'
 require 'panier/domain/receipt'
 require 'panier/domain/product'
 require 'panier/domain/tax_class'
+require 'panier/domain/product_service'
+require 'panier/domain/round_up_rounding'
 
+# application
 require 'panier/application/input_reader'
 require 'panier/application/cli'
 require 'panier/application/sales_tax_service'
+
+# decorators
+require 'panier/decorators/decorator'
+require 'panier/decorators/receipt_decorator'
 
 ##
 # The module containing all the panier code.
--- panier.gemspec
+++ panier.gemspec
@@ -18,7 +18,6 @@ Gem::Specification.new do |spec|
   spec.require_paths = ['lib']
 
   spec.add_dependency 'money'
-  spec.add_dependency 'workflow'
   spec.add_dependency 'activesupport'
 
   spec.add_development_dependency 'bundler', '~> 1.6'
